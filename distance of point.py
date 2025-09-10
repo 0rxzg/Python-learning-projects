dd53fbe954f9f4a2cd1110193efe51bd93cb9103
𝐋𝐗𝐅𝐅𝐘 𝐇𝐄𝐑𝐄 ~ <tarang2429@gmail.com>
2025-09-10 14:57:02 +0530

    Create distance of point.py

---
+++ distance of point.py
@@ -0,0 +1,8 @@
+x1=int(input("enter x1:"))
+y1=int(input("enter y1:"))
+x2=int(input("enter x2:"))
+y2=int(input("enter y2:"))
+x=(x2-x1)**2
+y=(y2-y1)**2
+distance=(x+y)**0.5
+print("distance between (",x1,",",y1,") and (",x2,",",y2,") is ", distance)
